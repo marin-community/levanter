61a6d803293d3890f2c12083bc5922bae2c080f8
Chi Heem W <chiheemwong@gmail.com>
2025-10-13 21:17:53 +0000

    Remove default stop string

--- src/levanter/eval_harness.py
+++ src/levanter/eval_harness.py
@@ -921,7 +921,7 @@ class TaskConfig:
     """Jinja2 template string to process a sample into a list of possible string choices for multiple_choice tasks. """
 
     # Extra Levanter-only config to control generation stops per task
-    additional_stop_strings: list[str] | None = field(default_factory=lambda: ['<|end_think|>'])
+    additional_stop_strings: list[str] | None = None
 
     def to_dict(self):
         base_dict = dataclasses.asdict(self)
