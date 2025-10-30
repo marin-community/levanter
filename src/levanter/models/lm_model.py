081e54c96efb6a3f0f9f19b5b8a4a4474fc3cca6
David Hall <david.hall@openathena.ai>
2025-10-30 14:54:47 -0700

    didn't i do this

--- src/levanter/models/lm_model.py
+++ src/levanter/models/lm_model.py
@@ -54,6 +54,7 @@ class LmExample(eqx.Module):
         if ignore_id is not None:
             # we don't compute loss for any tokens matching the ignore index
             ignore_mask = hax.roll(tokens, -1, Pos) != ignore_id
+            ignore_mask = ignore_mask.astype(loss_mask.dtype)
             loss_mask = loss_mask & ignore_mask.astype(loss_mask.dtype)
 
         loss_mask = loss_mask.astype(jnp.int32)
